1b0b3fa39de8de70c454a21e15b11e8568472a17
Massimiliano Adamo <maxadamo@gmail.com>
2019-11-03 13:53:17 +0100

    remove concat if ensure is absent

--- manifests/create/user.pp
+++ manifests/create/user.pp
@@ -96,17 +96,19 @@ define galera_proxysql::create::user (
       fail('hash galera_hosts not defined')
     }
   } else {
-    $concat_order = fqdn_rand(999999997, "${dbuser}${dbpass_wrap.unwrap}")+2
-    $concat_content = ",{
-    username = \"${dbuser}\"
-    password = \"${dbpass_wrap.unwrap}\"
-    default_hostgroup = 0
-    active = 1
-  }"
-    concat::fragment { "proxysql_cnf_fragment_${dbuser}_${dbpass_wrap}":
-      target  => '/etc/proxysql.cnf',
-      content => $concat_content,
-      order   => $concat_order;
+    if $ensure == present or $ensure == 'present' {
+      $concat_order = fqdn_rand(999999997, "${dbuser}${dbpass_wrap.unwrap}")+2
+      $concat_content = ",{
+      username = \"${dbuser}\"
+      password = \"${dbpass_wrap.unwrap}\"
+      default_hostgroup = 0
+      active = 1
+    }"
+      concat::fragment { "proxysql_cnf_fragment_${dbuser}_${dbpass_wrap}":
+        target  => '/etc/proxysql.cnf',
+        content => $concat_content,
+        order   => $concat_order;
+      }
     }
   }
 
