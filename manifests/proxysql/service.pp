b9b81036c616a8da47a7a0be79683c8de98a739e
Massimiliano Adamo <maxadamo@gmail.com>
2021-02-12 16:18:49 +0100

    fix wrong require in resources

--- manifests/proxysql/service.pp
+++ manifests/proxysql/service.pp
@@ -38,22 +38,14 @@ class galera_proxysql::proxysql::service ($limitnofile, $proxysql_package) {
     hasrestart => true,
     hasstatus  => true,
     provider   => 'systemd',
-    require    => File[
-      '/lib/systemd/system/proxysql.service',
-      '/etc/systemd/system/proxysql.service.d/file_limit.conf'
-    ];
+    require    => File['/etc/systemd/system/proxysql.service.d/file_limit.conf'];
   }
 
-  exec {
-    default:
-      refreshonly => true,
-      path        => '/usr/bin:/usr/sbin:/bin';
-    "${module_name}_daemon_reload":
-      command => 'systemctl daemon-reload',
-      before  => Service['proxysql'];
-    'kill_to_replace_init_script':
-      command => 'pkill -f -9 proxysql',
-      returns => [0, 1];
+  exec { "${module_name}_daemon_reload":
+    refreshonly => true,
+    path        => '/usr/bin:/usr/sbin:/bin',
+    command     => 'systemctl daemon-reload',
+    before      => Service['proxysql'];
   }
 
 }
