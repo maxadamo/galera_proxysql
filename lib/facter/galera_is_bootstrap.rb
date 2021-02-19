ed81f238f868d5e9bc62fe4488ca8df8e689c060
Massimiliano Adamo <maxadamo@gmail.com>
2021-02-19 12:02:03 +0100

    redirect fact output

--- lib/facter/galera_is_bootstrap.rb
+++ lib/facter/galera_is_bootstrap.rb
@@ -1,9 +1,9 @@
 Facter.add(:galera_is_bootstrap) do
   setcode do
-    if system('/usr/bin/systemctl status mysql@bootstrap')     
-      false
-    else
+    if system('/usr/bin/systemctl status mysql@bootstrap >/dev/null 2>&1')
       true
+    else
+      false
     end
   end
 end
