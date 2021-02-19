0d4bd4a7f6f5cc47933f5977f1e2093ea6ed3982
Massimiliano Adamo <maxadamo@gmail.com>
2021-02-19 11:34:25 +0100

    trying ps

--- lib/facter/galera_is_bootstrap.rb
+++ lib/facter/galera_is_bootstrap.rb
@@ -1,13 +1,8 @@
 Facter.add(:galera_is_bootstrap) do
   setcode do
     begin
-      Facter::Core::Execution.execute('/usr/bin/systemctl status mysql@bootstrap')
+      Facter::Core::Execution.execute('ps -ef | grep mysql@bootstrap')
     rescue Facter::Core::Execution::ExecutionFailure
-      service_status = 'DOWN'
-    else
-      service_status = 'UP'
-    end
-    if service_status == 'DOWN'
       false
     else
       true
