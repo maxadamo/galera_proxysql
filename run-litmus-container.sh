5f5c2247312ae30c61d06a5b43ed6c5499256666
Massimiliano Adamo <massimiliano.adamo@geant.org>
2021-05-17 23:35:56 +0200

    upgrade pdk

--- run-litmus-container.sh
+++ run-litmus-container.sh
@@ -16,3 +16,4 @@ docker exec -it litmusimage_centos_7-2222 yum install vim bash-completion less t
 echo "you can create the manifest inside the container and then you can run:"
 echo "puppet apply /root/manifest.pp --hiera_config=/etc/puppetlabs/code/environments/production/modules/galera_proxysql/hiera.yaml"
 echo -e "\e[1;49;92m==> script completed in:\e[0m ${SECONDS} seconds"
+
